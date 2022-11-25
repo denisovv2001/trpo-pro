3d345974e1d7601f997f5118ae12f7651912b6c7
Prospect Mira <117602115+Yansimatov@users.noreply.github.com>
2022-11-25 14:47:37 +0300

    Update pages-register-BD.php
    пофиксил адресс к хосту

--- pages-register-BD.php
+++ pages-register-BD.php
@@ -32,7 +32,7 @@
 
   //   проверки длинны
 
-  $mysql = new mysqli('mysql-182064.srv.hoster.ru','srv182064_pstgu','pstgu2022t','srv182064_pstgu_new');
+  $mysql = new mysqli('ssh-182064.srv.hoster.ru','srv182064_pstgu','pstgu2022','srv182064_pstgu_new');
   $mysql->query("INSERT INTO `personal data` (`login`, `password`, `nickname`, `sity`, `@mail`, `foto`) 
   VALUES('$name', '$password', '$username', '$sity', '$email', '$foto') ") ;
   $mysql->close();
