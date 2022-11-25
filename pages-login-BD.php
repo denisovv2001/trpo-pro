a62fffd01e271c3491bdd60dccf66f45758a0a2c
Ivan_Korolev <1korolev1vanya@gmail.com>
2022-11-25 20:52:02 +0300

    шапка

--- pages-login-BD.php
+++ pages-login-BD.php
@@ -6,19 +6,17 @@
   //   назвал переменные и настроил фильтры
 
 
-  $mysql = new mysqli ('ssh-182064.srv.hoster.ru','srv182064_pstgu','pstgu2022','srv182064_pstgu_new');
-  $result = $mysql->query("SELECT * FROM `personal data` WHERE `password` = '$password' AND `nickname` = '$username'");
+  $mysql = new mysqli ('mysql-182064.srv.hoster.ru','srv182064_pstgu','pstgu2022','srv182064_pstgu_new');
+  $result = $mysql->query("SELECT * FROM `personal data` WHERE  `password` = '$password' AND `nickname` = '$username'");
   $user = $result->fetch_assoc(); 
   
-  
-  if $user != $username{
-    echo "введи норамльно";
-  } 
-  // if(count($user) == null) {
-  //   echo "Nfrjq gjkmpjdfntm, yt yfqlty";
-  //   exit();
-  // }
-  setcookie( 'use' , $use );
+  if ($user['nickname'] == '' ){
+    print_r("Такой ползователь не найден");
+    exit();
+  }
+  else {
+    print_r("Вы успешно авторизовались");
+  }
 
 
 
