e0c46c0e71b7bb6ad54250776ea999e77d12da2f
Ivan_Korolev <1korolev1vanya@gmail.com>
2022-12-01 13:17:32 +0300

    1 файл для подкл к БД

--- forms/conect.php
+++ forms/conect.php
@@ -1,8 +1,8 @@
 <?php
    // $mysql = new mysqli('localhost', 'root', 'root', 'pstgu'); 
-    $mysqli = new mysqli('mysql-182064.srv.hoster.ru','srv182064_pstgu','pstgu2022','srv182064_pstgu_new');
+    $mysql = new mysqli('mysql-182064.srv.hoster.ru','srv182064_pstgu','pstgu2022','srv182064_pstgu_new');
     mysqli_set_charset($mysql, 'utf8'); //чинит кодировку
-    
+
 
     if(!$mysql){
         die('БД не подключена');
