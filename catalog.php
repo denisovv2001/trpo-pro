5b1837009f59676ac35b34ca4ca8b64d191cc68a
Ivan_Korolev <1korolev1vanya@gmail.com>
2022-12-01 13:07:20 +0300

    добавил поиск и 1 файл для подкл к БД

--- catalog.php
+++ catalog.php
@@ -1,5 +1,6 @@
 <?php
 include 'header.php'; //подключение шапки
+include 'forms/conect.php';
 ?>
 
 <main id="main" class="main">
@@ -8,78 +9,100 @@ include 'header.php'; //подключение шапки
   ?>
   <div class="container">
     <div class="row">
-      <h2> Каталог </h2>
+      <h1> Каталог </h1>
       <div class="col-lg-8">
 
         <!-- Строка поиска -->
-        <form class="search-form d-flex align-items-center mt-2" method="POST" action="#">
-          <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Поиск" aria-label="Поиск" aria-describedby="button-addon2">
-            <button class="btn btn-primary" type="button" id="button-addon2">Поиск</button>
+        <form class="search-form d-flex align-items-center mt-2 " method="POST" action="catalog-search-BD.php">
+          <div class="input-group mb-3 shadow p-0 mb-1 bg-body rounded">
+            <input type="search" name = "search" class="form-control " placeholder="Поиск" aria-label="Поиск" aria-describedby="button-addon2">
+            <button class="btn btn-primary" type="submit" id="button-addon2">Поиск</button>
           </div>
         </form>
       </div>
 
       <!-- Кнопка фильтр  -->
       <div class="col-lg-4 mt-2">
-        <div class="dropdown">
-          <button type="button" class="btn btn-primary dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside"> Фильтр </button>
+        <div class="dropdown" >
+          <button type="button" class="btn btn-primary dropdown-toggle shadow" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside"> Фильтр </button>
           <form class="dropdown-menu p-2 mt-2">
+            <!-- Возраст -->
             <p class="mt-3 mb-1"><strong>Возраст</strong></p>
             <div class="mb-2">
-              <div class="form-check p-1 pt-0 mx-1">
-
-                <input type="checkbox" id="contactChoice1" name="zero" checked>
-                <label for="contactChoice1" class="me-4">0+</label>
-
-                <input type="checkbox" id="contactChoice1" name="six">
-                <label for="contactChoice1">6+</label>
+              <div class="form-check p-1 pt-0 mx-4">
+                <!-- <label class="me-4"><input class = "form-check-input"type="checkbox" id="contactChoice1" name="zero"  > 0+</label>
+                <label><input type="checkbox" id="contactChoice1" name="six"> 6+</label>
                 <br>
-                <input type="checkbox" id="contactChoice1" name="twelve">
-                <label for="contactChoice1" class="me-3">12+</label>
-
-                <input type="checkbox" id="contactChoice1" name="eighteen">
-                <label for="contactChoice1">18+</label>
+                <label class="me-3"><input type="checkbox" id="contactChoice1" name="twelve"> 12+</label>
+                <label><input type="checkbox" id="contactChoice1" name="eighteen"> 18+</label> -->
+                <input class="form-check-input" type="radio" name="all_age" id="flexRadioDefault1">
+                <label class="form-check-label" for="flexRadioDefault1">
+                  выбрать всё
+                </label>
+                <br>
+                <input class="form-check-input" type="radio" name="six_age" id="flexRadioDefault1">
+                <label class="form-check-label" for="flexRadioDefault1">
+                  до 6+
+                </label>
+                <br>
+                <input class="form-check-input" type="radio" name="twelve_age" id="flexRadioDefault1">
+                <label class="form-check-label" for="flexRadioDefault1">
+                  до 12+
+                </label>
+                <br>
+                <input class="form-check-input" type="radio" name="ething_age" id="flexRadioDefault1">
+                <label class="form-check-label" for="flexRadioDefault1">
+                  до 18+
+                </label>
+                <br>
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                <label class="form-check-label" for="flexRadioDefault1">
+                  от 18+
+                </label>
               </div>
             </div>
-
-
+            <!-- Количество игроков -->
             <p class="mt-3 mb-1"><strong>Количество игроков</strong></p>
             <div class="mb-4">
               <div class="form-check p-1 pt-0 mx-1">
-
-                <input type="text" id="contactChoice1" name="contact" placeholder="мин" class="form-control  w-50 p-2">
-
-                <input type="text" id="contactChoice1" name="contact" placeholder="макс" class="form-control  w-50 p-2 mt-2 mb-0 ">
+                <input type="text" id="contactChoice1" name="min_player" placeholder="мин" class="form-control  w-50 p-2">
+                <input type="text" id="contactChoice1" name="max_player" placeholder="макс" class="form-control  w-50 p-2 mt-2 mb-0 ">
               </div>
-
+              <!-- Время игры -->
               <p class="mt-3 mb-1"><strong>Время игры</strong></p>
-              <div class="mb-4">
-                <div class="form-check p-1 pt-0 mx-1">
-
-
-                  <label for="contactChoice1" class="me-4">до 15 мин</label>
-                  <input type="checkbox" id="contactChoice1" name="eighteen">
-                  <br>
-
-                  <label for="contactChoice1" class="me-3">16 - 30 мин</label>
-                  <input type="checkbox" id="contactChoice1" name="eighteen">
-                  <br>
-
-                  <label for="contactChoice1" class="me-3">31 - 60 мин</label>
-                  <input type="checkbox" id="contactChoice1" name="eighteen">
-                  <br>
-
-                  <label for="contactChoice1" class="me-2">61 - 120 мин</label>
-                  <input type="checkbox" id="contactChoice1" name="eighteen">
-                  <br>
-
-                  <label for="contactChoice1" class="me-4">более 2 ч.</label>
-                  <input type="checkbox" id="contactChoice1" name="eighteen">
+            <div class="mb-4">
+              <div class="form-check p-1 pt-0 mx-1">
+                <input type="text" id="contactChoice1" name="min_time" placeholder="мин" class="form-control  w-50 p-2">
+                <input type="text" id="contactChoice1" name="max_time" placeholder="макс" class="form-control  w-50 p-2 mt-2 mb-0 ">
+              </div>
+            </div>
+            <!-- Город  -->
+            <p class="mt-3 mb-1"><strong>Время игры</strong></p>
+            <div class="mb-4">
+              <div class="form-check p-1 pt-0 mx-1">
+                <label for="city" class="form-label">Город</label>
+                <select class="form-select" id="city" required="" name="city_choice">
+                <option value="">Город...</option>
+                <option>Москва</option>
+                <option>Санкт-Петербург</option>
+                <option>Волгоград</option>
+                <option>Екатеринбург</option>
+                <option>Казань</option>
+                <option>Нижний Новгород</option>
+                <option>Новосибирск</option>
+                <option>Омск</option>
+                <option>Ростов-на-Дону</option>
+                <option>Самара</option>
+                <option>Уфа</option>
+                <option>Челябинск</option> 
+                </select>
+                <div class="invalid-feedback">
+                Пожалуйста, выберите действующий город.
                 </div>
               </div>
             </div>
 
+              
             <button type="submit" class="btn btn-primary ms-2 mb-1">Применить</button>
           </form>
         </div>
@@ -90,22 +113,20 @@ include 'header.php'; //подключение шапки
     <!-- Ссылка на пояснение: https://getbootstrap.su/docs/5.2/layout/grid/#ряд-колонок -->
     <div class="row row-cols-3">
       <?php
-
-      // $mysqli = new mysqli('localhost', 'root', 'root', 'pstgu'); 
-      
-      $mysqli = new mysqli('mysql-182064.srv.hoster.ru','srv182064_pstgu','pstgu2022','srv182064_pstgu_new');
-      mysqli_set_charset($mysqli, 'utf8'); //чинит кодировку
-      
-
-      $result = $mysqli->query('SELECT * FROM `game ad`'); //забираю данные из БД
+      $result = $mysql->query('SELECT * FROM `game ad`'); //забираю данные из БД
 
       while ($row = $result->fetch_assoc()) // получаем все строки в цикле по одной
       {
         $name_game = $row['game name']; //название игры
         $city = $row['sity']; //город
-        $barter = 'бесплатно'; //способ обмена
+        $barter = $row['present']; //способ обмена настолкой
+        if($barter == 1)
+          $barter = "бесплатно";
+        else 
+          $barter = "обмен";
+
         echo '<div class="col mt-3 col-12-sm text-start" style="border-radius: 20px;">
-            <div class="card card_game" style="width: 18rem;">
+            <div class="card card_game shadow-lg " style="width: 18rem;">
             <a href="product_card.php" class="btn"> <img src="assets/img/game_of_trons.jpg" class="card-img-top" alt="..."></a>
             <div class="card-body text-start"> 
             <h5 class="card-title fw-weight-bolder text-capitalize">' . $name_game . '</h5>
@@ -116,10 +137,9 @@ include 'header.php'; //подключение шапки
           </div> 
           </div>';
       }
-
-
+      
       $result->free();
-      $mysqli->close();
+      $mysql->close();
       ?>
     </div>
 </main>
