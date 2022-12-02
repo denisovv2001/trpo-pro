b8f3d335f54993493227aaaaed85e855205e7045
Василий Денисов <vasyaasz@mail.ru>
2022-12-02 12:24:20 +0300

    тех. поддержка. Загрузка в БД почты и комментария выполняется (доработать: 1. Добавлять в БД тему вопроса; 2. Кнопку возврата в тех. поддержку после загрузки данных в БД; 3. Отобразить список заданных вопросов по темам на сайте из БД; 4. Отобразить список моих вопросов)

--- pages-contact.php
+++ pages-contact.php
@@ -1,4 +1,5 @@
 <?php 
+  // Файл pages-contact.php отображает страницу технической поддержки
   include 'header.php';
   include 'side-menu.php';
 ?>
@@ -7,15 +8,64 @@
 
 <main id="main" class="main">
 
-<div class="container ">
-    <div class="mb-3">
-    <label for="exampleFormControlInput1" class="form-label">Адрес электронной почты</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-    </div>
-    <div class="mb-3">
-    <label for="exampleFormControlTextarea1" class="form-label">Пример текстового поля</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    </div>
+  <div class="container">
+        <div class="row g-5">
+              <div class="col-md-12 col-lg-12">
+                <form class="needs-validation" novalidate="">
+                  <div class="row g-3">
 
-    <button type="button" class="btn btn-primary">Отправить</button>
-</main
+                    <h2> Техническая поддержка </h2>
+
+                    <!-- Отмечаем на выбор, тему запроса -->
+                    <div class="col-md-6 col-lg-6">
+                        <div class="row g-3">
+                          <div class="col-sm-10">
+                            <label for="city" class="form-label">Выберите тему обращения</label>
+                            <select class="form-select" id="city" required="">
+                              <option value="">Не работает элемент сайта</option>
+                              <option>Не пускает с логином и паролем на сайт</option>
+                              <option>Проблемы сети</option>
+                              <option>Недоступен сервер</option>
+                              <option>Сайт недоступен (но сервер доступен)</option>
+                              <option>Другое</option>
+                            </select>
+                            <div class="invalid-feedback">
+                              Пожалуйста, выберите тему запроса.
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                </form>
+              </div>
+        </div>
+  </div>
+
+  <!-- переход из pages-contact.php в pages-contact-BD.php с помощью метода post -->
+  <form action="pages-contact-BD.php" method="post">
+    <div class="container ">
+      
+      <br>
+
+      <!-- поле ввода электронной почты -->
+      <div class="col-sm-8">
+        <label for="exampleFormControlInput1" class="form-label">Укажите Вашу электронную почту</label>
+        <input type="email" name="email" class="form-control" id="exampleFormControlInput1" placeholder="пример: name@example.com">
+      </div>
+      
+      <br>
+
+      <!-- поле ввода текста возникшей проблемы -->
+      <div class="col-sm-10">
+        <label for="exampleFormControlTextarea1" class="form-label">Как воспроизводится проблема и о какой услуге речь</label>
+        <textarea name="text" class="form-control" id="exampleFormControlTextarea1" placeholder="Опишите свою проблему по существу, без длительных вступлений и «воды», но с указанием всех необходимых данных." rows="3"></textarea>
+      </div>
+      
+      <br><br>
+
+      <!-- по нажатии будет произведена попытка записи email и text в БД -->
+      <button class="btn btn-primary w-75" type="submit">Отправить</button>
+    </div>
+  </form>
+
+</main>
