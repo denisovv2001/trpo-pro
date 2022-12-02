b911e424f1311763e7a694421cbc853e2763425f
DanTarasevskiy <soundwaveDT@yandex.ru>
2022-12-03 01:53:23 +0300

    Сделал Создать объявление

--- new_game_to_shop.php
+++ new_game_to_shop.php
@@ -3,6 +3,9 @@
   include 'side-menu.php';
 ?>
 
+<!DOCTYPE html>
+<html>
+
 <body>
 
 <main id="main" class="main">
@@ -10,146 +13,152 @@
 <div class="container">
         <div class="row g-5">
               <div class="col-md-12 col-lg-12">
-                <form class="needs-validation" novalidate="">
-                  <div class="row g-3">
+                
+                
+                <form action="new_game_to_shop-BD.php" method="post" enctype="multipart/form-data">
+                  <form class="needs-validation" novalidate="">
+                    <div class="row g-3">
+                      <form action="new_game_to_shop-BD.php" method="post" enctype="multipart/form-data">
 
-                    <h2> Создать объявление </h2>
+                        <h2> Создать объявление </h2>
 
-                    <div class="col-md-6 col-lg-6">
-                      
-                        <div class="row g-3">
-
-                          <div class="col-sm-6">
-                            <label for="NameTheGame" class="form-label">Название игры</label>
-                            <input type="text" class="form-control" id="NameTheGame" placeholder="" value="" required="">
-                            <div class="invalid-feedback">
-                              Название игры обязательно.
-                            </div>
-                          </div>
-
-                          <!-- Заполняем город на выбор, в кот. находится игра при создании объявления -->
-                          <div class="col-md-5">
-                            <label for="city" class="form-label">Город</label>
-                            <select class="form-select" id="city" required="">
-                              <option value="">Город...</option>
-                              <option>Moscow</option>
-                              <option>Saint-Petersburg</option>
-                              <option>Novosibirsk</option>
-                              <option>Ekaterinburg</option>
-                              <option>Kazan</option>
-                              <option>Chelyabinsk</option>
-                              <option>Krasnodar</option>
-                            </select>
-                            <div class="invalid-feedback">
-                              Пожалуйста, выберите действующий город.
-                            </div>
-                          </div>
-
-                          <label for="age" class="form-label">Возраст игрока от:</label>
-
-                          <div class="col-md-2">
-                            <label class="form-check-label" for="same-address">0+</label>
-                            <input type="checkbox" class="form-check-input" id="same-address">
-                          </div>
-
-                          <div class="col-md-2">
-                            <label class="form-check-label" for="same-address">6+</label>
-                            <input type="checkbox" class="form-check-input" id="same-address">
-                          </div>
-
-                          <div class="col-md-2">
-                            <label class="form-check-label" for="same-address">12+</label>
-                            <input type="checkbox" class="form-check-input" id="same-address">
-                          </div>
-
-                          <div class="col-md-2">
-                            <label class="form-check-label" for="same-address">18+</label>
-                            <input type="checkbox" class="form-check-input" id="same-address">
-                          </div>
-
-                          <div class="col-md-6">
-                            <label for="numberPlayers" class="form-label">Кол-во игроков:</label>
-                          </div>
-
-                          <div class="col-md-3">
-                            <label for="minNumberPlayers" class="form-label">min</label>
-                            <input type="text" class="form-control" id="minNumberPlayers" placeholder="" required="">
-                            <div class="invalid-feedback">
-                              Введите минимальное количество игроков.
-                            </div>
-                          </div>
-
-                          <div class="col-md-3">
-                            <label for="maxNumberPlayers" class="form-label">max</label>
-                            <input type="text" class="form-control" id="maxNumberPlayers" placeholder="" required="">
-                            <div class="invalid-feedback">
-                              Введите максимальное количество игроков.
-                            </div>
-                          </div>
-
-                          <div class="col-md-5">
-                            <label for="gameTime" class="form-label">Время игры</label>
-                            <select class="form-select" id="gameTime" required="">
-                              <option value="">...</option>
-                              <option>до 15 мин</option>
-                              <option>16-30 мин</option>
-                              <option>31-60 мин</option>
-                              <option>1-2 часа</option>
-                              <option>более 2-ух часов</option>
-                            </select>
-                            <div class="invalid-feedback">
-                              Пожалуйста, выберите время игры.
-                            </div>
-                          </div>
-
-                          <div class="my-3">
-                            <label for="exchangeOrGift" class="form-label">Обмен/подарок</label>
-                            <div class="form-check">
-                              <input id="exchange" name="paymentMethod" type="radio" class="form-check-input" checked="" required="">
-                              <label class="form-check-label" for="exchange">Обмен</label>
-                            </div>
-                            <div class="form-check">
-                              <input id="gift" name="paymentMethod" type="radio" class="form-check-input" required="">
-                              <label class="form-check-label" for="gift">Подарок</label>
-                            </div>
-                          </div>
-
+                        <div class="col-md-6 col-lg-6">
                           
+                            <div class="row g-3">
+
+                              <div class="col-sm-6">
+                                <label for="NameTheGame" class="form-label"><strong>Название игры</strong></label>
+                                <input type="text" class="form-control" name="gamename" id="NameTheGame" required>
+                                <div class="invalid-feedback">
+                                  Название игры обязательно.
+                                </div>
+                              </div>
+
+                              
+                              <div class="col-md-5">
+                                <label for="NameTheGame" class="form-label"><strong>Город</strong></label>
+                                <select class="form-select" name="sity" id="city">
+                                  <option>Город...</option>
+                                  <option value = "Moscow">Moscow</option>
+                                  <option value = "Saint-Petersburg">Saint-Petersburg</option>
+                                  <option value = "Novosibirsk">Novosibirsk</option>
+                                  <option value = "Ekaterinburg">Ekaterinburg</option>
+                                  <option value = "Kazan">Kazan</option>
+                                  <option value = "6">Chelyabinsk</option>
+                                  <option value = "7">Krasnodar</option>
+                                </select>
+                                <div class="invalid-feedback">
+                                  Пожалуйста, выберите действующий город.
+                                </div>
+                              </div>
+
+
+                              <div class="col-md-6 col-lg-6">
+                                <label for="NameTheGame" class="form-label"><strong>Время игры</strong></label>                        
+                                <div class="form-check p-1 pt-0 mx-1">
+                                  <input type="text" id="contactChoice1" name="mintime" placeholder="мин" class="form-control">
+                                  <input type="text" id="contactChoice1" name="maxtime" placeholder="макс" class="form-control">
+                                </div>
+                              </div>
+
+
+                              <div class="col-md-5 col-lg-5">
+                                <div class="row">
+                                <label for="NameTheGame" class="form-label"><strong>Количество игроков</strong></label>                        
+                                <div class="form-check p-1 pt-0 mx-1">
+                                  <input type="text" id="contactChoice2" name="qualitymin" placeholder="мин" class="form-control">
+                                  <input type="text" id="contactChoice2" name="qualitymax" placeholder="макс" class="form-control">
+                                </div>
+                                </div>
+                              </div>
+
+
+                              <div class="col-md-6 col-lg-6">
+                                <label for="NameTheGame" class="form-label"><strong>Минимальный возраст</strong></label>
+                                <div class="form-check p-1 pt-0 mx-1">
+                                  <input type="text" id="contactChoice1" name="age" placeholder="Например - 7" class="form-control  w-65 p-2" required>
+                                  <div class="invalid-feedback">
+                                    Возраст.
+                                  </div>
+                                </div>
+                              </div>
+
+                            <!--
+                              <div class="my-3 col-md-6 col-lg-6">
+                                <label for="NameTheGame" class="form-label"><strong>Обмен/Подарок</strong></label>
+                                <div class="form-check">
+                                  <input id="exchange" name="present" type="radio" class="form-check-input">
+                                  <label class="form-check-label" value="0">Обмен</label>
+                                </div>
+                                <div class="form-check">
+                                  <input id="gift" name="present" type="radio" class="form-check-input">
+                                  <label class="form-check-label" value="1">Подарок</label>
+                                </div>
+                              </div> -->
+
+                              <div class="col-md-5">
+                                <label for="NameTheGame" class="form-label"><strong>Обмен/Бесплатно</strong></label>
+                                <select class="form-select" name="present" id="p">
+                                  <option>...</option>
+                                  <option value = "1">Бесплатно</option>
+                                  <option value = "0">Обмен</option>
+                                </select>
+                                <div class="invalid-feedback">
+                                  Пожалуйста, выберите бесплатно или обмен.
+                                </div>
+                              </div>
+
+                            </div>
 
                         </div>
-                      
-                    </div>
 
 
-                    
-                      <div class="col-md-6 col-lg-6 col-sm-12">
-                       
-                          <div class="row g-3">
+                        
+                          <div class="col-md-6 col-lg-6 col-sm-12">
+                          
+                              <div class="row g-3">
 
-                            <img src="assets/img/game_of_trons.jpg" alt="Game" class="card-img-top" style="width: 300px">
-                           
-                              <div class="d-flex justify-content-center pt-2">
-                                <a href="#" class="btn btn-primary btn-sm mx-2" title="Upload new game image"><i class="bi bi-upload"></i></a>
-                                <a href="#" class="btn btn-danger btn-sm " title="Remove game image"><i class="bi bi-trash"></i></a>
+                                
+                                <input class="form-control w-75" type="file" name="foto1">
+                                <input class="form-control w-75" type="file" name="foto2">
+                                <input class="form-control w-75" type="file" name="foto3">
+
+                              
+                                  
+                                <textarea type="text" name="comment"class="form-control" placeholder="Описание к игре" id="floatingTextarea" style="height: 177px;"></textarea>
+                                
+
                               </div>
                             
-
-                          </div>
+                          </div>    
                         
-                      </div>    
-                    
-                    <div class="col-lg-6  col-md-12 d-flex">
-                    
+                        <div class="col-lg-6  col-md-12 d-flex">
+                          
+                            <button class="w-80 btn btn-primary btn-lg container-fluid" type="submit">Создать объявление</button>
+
+                        </div>
+
                       
-                        <button class="w-80 btn btn-primary btn-lg container-fluid" type="submit">Создать объявление</button>
-      
-                    
                     </div>
-
-
-                  </div>
+                  </form>
                 </form>
               </div>
         </div>
 </div>
-</main
+</main>
+
+<script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/chart.js/chart.min.js"></script>
+<script src="assets/vendor/echarts/echarts.min.js"></script>
+<script src="assets/vendor/quill/quill.min.js"></script>
+<script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="assets/vendor/tinymce/tinymce.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
--- product_card.php
+++ product_card.php
@@ -5,79 +5,95 @@
 
 <body>
 
-<main id="main" class="main">
-<body>
+  <main id="main" class="main">
+    <?php
 
-  <div class="container ">
-  <div class="row">
-    <div class="col-7">
-      <h2> Название игры </h2>
-    </div>
+    $result = $mysql->query('SELECT * FROM `game ad`');
+    while ($row = $result->fetch_assoc()) // получаем все строки в цикле по одной
+      {
+        $users_id = $row['users_id']
 
-    <div class="col-1 mb-3">
-      <a class="btn btn-primary" href="#" role="button"><i class="bi bi-bookmark"></i></a>
-    </div>
-    </div>
+        if(getmyuid() == $users_id)
+        $name_game = $row['game_name']; //название игры
+        $city = $row['sity']; //город
+        $barter = $row['present']; //способ обмена настолкой
+        if($barter == 1)
+          $barter = "бесплатно";
+        else 
+          $barter = "обмен";
+    echo '<body>
 
-  <div class="row">
-    <div class="col-8">
-        
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <div class="container ">
+        <div class="row">
+          <div class="col-7">
+            <h2> Название игры </h2>
           </div>
-        
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="assets/img/game_of_trons.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="assets/img/game_of_trons2.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="assets/img/game_of_trons3.png" class="d-block w-100" alt="...">
-            </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Предыдущий</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Следующий</span>
-          </button>
-        </div>
 
-      </div>
-
-      <div class="col-4 col-sm">
-        <h2 class="fs-2 text-center "><strong>БЕСПЛАТНО<i class="bi-gift ms-2"></i></strong></h2>
-
-        <div class="card" style="width: 18rem;">  
-          <div class="card-body">
-            <h5 class="card-title">Имя Фамилия</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Москва</h6>
-            <p class="card-text">Номер телефона</p>
-            <p class="card-text">Адрес почты</p>
+          <div class="col-1 mb-3">
+            <a class="btn btn-primary" href="#" role="button"><i class="bi bi-bookmark"></i></a>
           </div>
         </div>
 
-        <div class="card">
-          <div class="card-body">
-            Некий текст амукмйиутвстйоцтвостйцотсойцтсжицисшршм й урц сшц ув  в р мйтуковтотфс й усто
+        <div class="row">
+          <div class="col-8">
+              
+              <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+              
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="assets/img/game_of_trons.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="assets/img/game_of_trons2.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="assets/img/game_of_trons3.png" class="d-block w-100" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Предыдущий</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Следующий</span>
+                </button>
+              </div>
+
+            </div>
+
+            <div class="col-4 col-sm">
+              <h2 class="fs-2 text-center "><strong>БЕСПЛАТНО<i class="bi-gift ms-2"></i></strong></h2>
+
+              <div class="card" style="width: 18rem;">  
+                <div class="card-body">
+                  <h5 class="card-title">Имя Фамилия</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Москва</h6>
+                  <p class="card-text">Номер телефона</p>
+                  <p class="card-text">Адрес почты</p>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-body">
+                  Некий текст амукмйиутвстйоцтвостйцотсойцтсжицисшршм й урц сшц ув  в р мйтуковтотфс й усто
+                </div>
+              </div>
+            </div>
+
           </div>
-        </div>
-      </div>
+        </body>
+      </main>
 
-    </div>
-
-</main>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
-  </body>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+    </body>';
+    ?>
 </body>
 </html>
