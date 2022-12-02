ba0e94ef23f8447008c724d0efb3d3ea18d9a563
Prospect Mira <117602115+Yansimatov@users.noreply.github.com>
2022-12-02 20:54:15 +0300

    правил главное меню для не  зашедшего

--- login-error.php
+++ login-error.php
@@ -6,6 +6,7 @@
 
 <body>
 
+
 <main id="main" class="main">
 
 <div class="container ">
@@ -15,8 +16,8 @@
       <div class="card">
   
   <div class="card-body">
-    <h5 class="card-title">Незарегистрированные пользователи не имеют доступа к полному функцианалу сайта.</h5>
-    <p class="card-text">Пожалуйста зарегистрируйтесь или войдите.</p>
+    <h5 class="card-title">Такой пользователь не найден. Пожалуйста, проверте логин и пароль.</h5>
+    <p class="card-text">Пожалуйста зарегистрируйтесь или  попробуйте войдити еще раз .</p>
     <a href="pages-register.php" class="btn btn-primary">Регистрация</a>
     <a href="pages-login.php" class="btn btn-primary">Вход</a>
   </div>
