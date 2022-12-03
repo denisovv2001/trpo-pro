407fd0d3d9ffbd80256b5fb2efbe7905a6c9cbf4
redbox12 <61825006+redbox12@users.noreply.github.com>
2022-12-03 09:14:15 +0300

    Update conect.php

--- forms/conect.php
+++ forms/conect.php
@@ -1,6 +1,6 @@
 <?php
-    $mysql = new mysqli('localhost', 'root', 'root', 'pstgu'); 
-    //$mysql = new mysqli('mysql-182064.srv.hoster.ru','srv182064_pstgu','pstgu2022','srv182064_pstgu_new');
+   // $mysql = new mysqli('localhost', 'root', 'root', 'pstgu'); 
+    $mysql = new mysqli('mysql-182064.srv.hoster.ru','srv182064_pstgu','pstgu2022','srv182064_pstgu_new');
     mysqli_set_charset($mysql, 'utf8'); //чинит кодировку
 
 
