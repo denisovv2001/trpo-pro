974159d046f06dbf58c4cc98281cb5711935c06d
redbox12 <61825006+redbox12@users.noreply.github.com>
2022-12-03 09:53:30 +0300

    Update header.php

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
 <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.php" class="logo d-flex align-items-center">
+      <a href="catalog.php" class="logo d-flex align-items-center">
       <img src="assets/img/logo.png" class="logo-game" alt="">
         <span class="d-none d-lg-block mx-2"> Мир БУ настолок</span>
       </a>
